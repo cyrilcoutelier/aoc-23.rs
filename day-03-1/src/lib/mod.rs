f32dec5be4d3dc2d339e040db0dcc14e5cd8c55a
Cyril Coutelier <cyril@flat.io>
2023-12-04 23:40:24 +0100

    feat: day 03-1

--- day-03-1/src/lib/mod.rs
+++ day-03-1/src/lib/mod.rs
@@ -1,6 +1,134 @@
-pub fn process_lines<T>(_lines: T) -> i32
+use std::collections::HashSet;
+
+const RADIX: u32 = 10;
+
+#[derive(Eq, PartialEq, Debug, Clone)]
+pub struct PartNumber {
+    pub value: usize,
+    pub start: isize,
+    pub end: isize,
+}
+
+impl PartNumber {
+    fn new(digit: usize, start: isize) -> Self {
+        let end = start;
+        PartNumber {
+            value: digit,
+            start,
+            end,
+        }
+    }
+
+    fn add_digit(&mut self, digit: usize) {
+        self.end += 1;
+        self.value *= 10;
+        self.value += digit;
+    }
+}
+
+pub type LineSymbols = HashSet<isize>;
+
+#[derive(Eq, PartialEq, Debug)]
+pub struct LineData {
+    pub part_numbers: Vec<PartNumber>,
+    pub line_symbols: LineSymbols,
+}
+
+fn is_matching(part_number: &PartNumber, line_symbols: &LineSymbols) -> bool {
+    (part_number.start - 1..=part_number.end + 1).any(|pos| line_symbols.contains(&pos))
+}
+
+enum Entry {
+    Void,
+    Symbol(char),
+    Number(usize),
+}
+
+impl From<char> for Entry {
+    fn from(c: char) -> Entry {
+        match c {
+            '.' => Entry::Void,
+            '0'..='9' => Entry::Number(c.to_digit(RADIX).unwrap() as usize),
+            _ => Entry::Symbol(c),
+        }
+    }
+}
+
+pub fn parse_line(line: &str) -> LineData {
+    let mut part_numbers = Vec::new();
+    let mut line_symbols = LineSymbols::new();
+    let mut current_part_number: Option<PartNumber> = None;
+
+    line.chars()
+        .map(Entry::from)
+        .enumerate()
+        .for_each(|(index, entry)| match entry {
+            Entry::Number(digit) => {
+                if let Some(part_number) = current_part_number.as_mut() {
+                    part_number.add_digit(digit);
+                } else {
+                    current_part_number = Some(PartNumber::new(digit, index as isize));
+                }
+            }
+            Entry::Symbol(_) => {
+                if let Some(part_number) = current_part_number.take() {
+                    part_numbers.push(part_number);
+                }
+                line_symbols.insert(index as isize);
+            }
+            Entry::Void => {
+                if let Some(part_number) = current_part_number.take() {
+                    part_numbers.push(part_number);
+                }
+            }
+        });
+
+    if let Some(part_number) = current_part_number.take() {
+        part_numbers.push(part_number);
+    }
+
+    LineData {
+        part_numbers,
+        line_symbols,
+    }
+}
+
+pub fn process_lines<T>(lines: T) -> i32
 where
     T: Iterator<Item = String>,
 {
-    0
+    let mut parts = Vec::new();
+    let mut previous_line_data: Option<LineData> = None;
+    lines.map(|line| parse_line(&line)).for_each(|line_data| {
+        parts.extend(
+            line_data
+                .part_numbers
+                .iter()
+                .filter(|part_number| is_matching(part_number, &line_data.line_symbols))
+                .cloned(),
+        );
+
+        if let Some(previous_line_data) = previous_line_data.take() {
+            parts.extend(
+                line_data
+                    .part_numbers
+                    .iter()
+                    .filter(|part_number| {
+                        is_matching(part_number, &previous_line_data.line_symbols)
+                    })
+                    .cloned(),
+            );
+            parts.extend(
+                previous_line_data
+                    .part_numbers
+                    .iter()
+                    .filter(|part_number| is_matching(part_number, &line_data.line_symbols))
+                    .cloned(),
+            );
+        }
+        previous_line_data = Some(line_data);
+    });
+
+    let result: usize = parts.iter().map(|part| part.value).sum();
+    result as i32
 }
--- day-03-1/tests/lib_test.rs
+++ day-03-1/tests/lib_test.rs
@@ -1,15 +1,121 @@
+mod parse_line {
+    use aoc2023::{parse_line, LineData, LineSymbols, PartNumber};
+
+    #[test]
+    fn two_numbers() {
+        // When
+        let line = "..123..234..";
+
+        // When
+        let result = parse_line(line);
+
+        // Then
+        assert_eq!(
+            result,
+            LineData {
+                part_numbers: vec![
+                    PartNumber {
+                        value: 123,
+                        start: 2,
+                        end: 4
+                    },
+                    PartNumber {
+                        value: 234,
+                        start: 7,
+                        end: 9
+                    },
+                ],
+                line_symbols: LineSymbols::new()
+            }
+        );
+    }
+
+    #[test]
+    fn two_number_and_symbol() {
+        // When
+        let line = ".12#34.";
+
+        // When
+        let result = parse_line(line);
+
+        // Then
+        let mut line_symbols = LineSymbols::new();
+        line_symbols.insert(3);
+
+        assert_eq!(
+            result,
+            LineData {
+                part_numbers: vec![
+                    PartNumber {
+                        value: 12,
+                        start: 1,
+                        end: 2
+                    },
+                    PartNumber {
+                        value: 34,
+                        start: 4,
+                        end: 5,
+                    },
+                ],
+                line_symbols,
+            }
+        );
+    }
+
+    #[test]
+    fn two_numbers_at_end() {
+        // When
+        let line = ".12.34.";
+
+        // When
+        let result = parse_line(line);
+
+        // Then
+        assert_eq!(
+            result,
+            LineData {
+                part_numbers: vec![
+                    PartNumber {
+                        value: 12,
+                        start: 1,
+                        end: 2
+                    },
+                    PartNumber {
+                        value: 34,
+                        start: 4,
+                        end: 5,
+                    },
+                ],
+                line_symbols: LineSymbols::new()
+            }
+        );
+    }
+}
+
 mod process_lines {
     use aoc2023::process_lines;
 
     #[test]
     fn puzzle_example() {
         // When
-        let input = vec!["Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".to_string()];
+        let input = vec![
+            "467..114..".to_string(),
+            "...*......".to_string(),
+            "..35..633.".to_string(),
+            "......#...".to_string(),
+            "......#...".to_string(),
+            "617*......".to_string(),
+            ".....+.58.".to_string(),
+            "..592.....".to_string(),
+            "......755.".to_string(),
+            "...$.*....".to_string(),
+            ".664.598..".to_string(),
+        ];
 
         // When
         let result = process_lines(input.into_iter());
 
         // Then
-        assert_eq!(result, 0);
+        assert_eq!(result, 4361);
     }
 }
